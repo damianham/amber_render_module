6eee1743765fbba594edd1350270e5bd970a6ca9
Damian Hamill <damianham@gmail.com>
2020-04-09 15:04:15 +0700

    add render_module macro to Quartz::Composer

--- src/amber_render_module.cr
+++ src/amber_render_module.cr
@@ -9,3 +9,9 @@ end
 class ApplicationController < Amber::Controller::Base
   include Amber::Controller::Helpers::RenderModule
 end
+
+# patch the Quartz::Composer class to include the render_module macro
+class Quartz::Composer
+  include Amber::Controller::Helpers::RenderModule
+end
+
